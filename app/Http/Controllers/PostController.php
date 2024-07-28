81f7bca2f151a0292c15e77cfa2b3144ae48b57d
Sebastian152 <sgalindorks@gmail.com>
2024-07-28 14:08:40 -0600

    Adding Crud first steps

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('posts.index');
+        // Retrieving all posts
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
     }
 
     public function create() {
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Tailwind</title>
-</head>
-<body>
+<x-app-layout title="Blog posts">
+
     <h1>Aquí se mostrarán todos los posts</h1>
-</body>
-</html>
+        @foreach ($posts as $post)
+            <li class="ml-4">
+                <a href="/posts/{{$post->id}}">
+                    {{ $post->title }}
+                </a>
+            </li>
+        @endforeach
+</x-app-layout>
